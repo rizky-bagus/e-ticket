8a99fb1fa0b6ec5edc847cdf872d22606a2c0b71
theoz06 <t.ziraluo@yahoo.com>
2022-06-11 22:15:04 +0700

    Halaman Login

---
+++ application/modules/login/views/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/styleLogin.css">
+  </head>
+  <body>
+<div class="login-box">
+  <h1>Login</h1>
+  <form name="Login">
+      <div class="textbox">
+       <div class="input-area">
+         <i class="fas fa-user"></i>
+         <input type="text" placeholder="Username" required>
+         <small>Error message</small>
+        </div>
+      </div>
+    <div class="textbox">
+      <div class=" input-area">
+        <i class="fas fa-lock"></i>
+        <input type="password" placeholder="Password" required>
+        <small>Error message</small>
+      </div>
+    </div>
+    <button>Submit</button>
+    <p class="sign-up">Belum punya akun? <a href="Registrasi.html">Buat Sekarang</a></p></div>
+  </form>
+  </body>
+</html>
